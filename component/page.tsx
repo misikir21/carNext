b77c7a347a3ae0301005fef9c5a2ffa433fcc9a8
misikir21 <misikirkaka@gmail.com>
2023-11-10 15:25:35 +0300

    fix typescriot cannot find name div issues

---
+++ component/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Hero } from "@/component";
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  );
+}
