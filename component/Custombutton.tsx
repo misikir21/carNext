bb84493d615612172331ac652920dac2d50a8af9
misikir21 <misikirkaka@gmail.com>
2023-11-15 15:24:16 +0300

    add project asset folder

--- component/Custombutton.tsx
+++ component/Custombutton.tsx
@@ -1,36 +1,22 @@
 "use client";
-
+import { CustombuttonProps } from "@/types";
 import Image from "next/image";
 
-import { CustomButtonProps } from "@types";
-
-const Button = ({
-  isDisabled,
-  btnType,
-  containerStyles,
-  textStyles,
+const Custombutton = ({
   title,
-  rightIcon,
+  containerStyles,
   handleClick,
-}: CustomButtonProps) => (
-  <button
-    disabled={isDisabled}
-    type={btnType || "button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
-  >
-    <span className={`flex-1 ${textStyles}`}>{title}</span>
-    {rightIcon && (
-      <div className="relative w-6 h-6">
-        <Image
-          src={rightIcon}
-          alt="arrow_left"
-          fill
-          className="object-contain"
-        />
-      </div>
-    )}
-  </button>
-);
+}: CustombuttonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
 
-export default Button;
+export default Custombutton;
