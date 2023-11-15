c99707da142d11925b35a568ccd986827c3ba3d1
misikir21 <misikirkaka@gmail.com>
2023-11-15 16:48:18 +0300

    update the layout jsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative">
-        <NavBar />
+        <Navbar />
         {children}
         <Footer />
       </body>
