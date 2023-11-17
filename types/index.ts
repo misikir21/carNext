833f650ce06596aa4b61c90e9655969db7da6e01
misikir21 <misikirkaka@gmail.com>
2023-11-17 09:05:14 +0300

    create a btntype

--- types/index.ts
+++ types/index.ts
@@ -2,5 +2,6 @@ import { MouseEventHandler } from "react";
 export interface CustombuttonProps {
   title: string;
   containerStyles?: string;
+  btnType?: "button" | "submit";
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
