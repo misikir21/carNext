64d457fedb0e449879e4dabd28bb471ebfe3c34a
misikir21 <misikirkaka@gmail.com>
2023-11-17 08:53:58 +0300

    add a logo to the header

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
+import { Custombutton } from ".";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full absolute z-10">
+      <nav
+        className="max-w-[144px] mx-auto flex justify-between items-center
+ sm:px-16 px-6 py-4"
+      >
+        <link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="car hub logo"
+            width={118}
+            height={118}
+            className="object-contain"
+          />
+        </link>
+      </nav>
+    </header>
+  );
 };
-
-export default Navbar;
