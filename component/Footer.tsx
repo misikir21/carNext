aa3b32d57861f6b308643e5612998434ef6ab7f7
misikir21 <misikirkaka@gmail.com>
2023-11-17 09:35:17 +0300

    add copyright to the footer section

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100 ">
       <div className="flex max-md:flex-col flex-wrap justify-between gpa-5 sm:px-16 px-6 py-10">
-        <div className="flex flex-col justify-start items-start gap-6">
+        <div className="flex flex-col justify-start items-start gap-16">
           <Image
             src="/logo.svg"
             alt="logo"
@@ -12,6 +12,10 @@ const Footer = () => {
             height={18}
             className="object-contain"
           />
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
         </div>
       </div>
     </footer>
