615012c4822f74163f9a42e752db9ad87a5b2e34
misikir21 <misikirkaka@gmail.com>
2023-11-17 09:13:43 +0300

    update the custombutton props

--- component/Custombutton.tsx
+++ component/Custombutton.tsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 
 const Custombutton = ({
   title,
+  btnType,
   containerStyles,
   handleClick,
 }: CustombuttonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
