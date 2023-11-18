62529338b36b6a740b7688e3f22b57b53533a52e
misikir21 <misikirkaka@gmail.com>
2023-11-18 10:17:47 +0300

    add custom comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Hero } from "@/component";
+import { Hero, CustomFilter, SearchBar } from "@/component";
 export default function Home() {
   return (
     <main className="overflow-hidden">
--- component/index.ts
+++ component/index.ts
@@ -1,5 +1,7 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 import Custombutton from "./Custombutton";
-export { Navbar, Footer, Hero, Custombutton };
+export { Navbar, SearchBar, CustomFilter, Footer, Hero, Custombutton };
